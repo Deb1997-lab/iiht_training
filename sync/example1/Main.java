3095bf93efcfa9fb52db14a0f24ac8c207ba7611
Deb1997-lab <71452256+Deb1997-lab@users.noreply.github.com>
2023-07-04 12:40:01 +0530

    Update Main.java

--- sync/example1/Main.java
+++ sync/example1/Main.java
@@ -4,21 +4,35 @@ import sync.example1.model.*;
 public class Main {
 	public static void main(String[] args) {
 		SynchronisedStack<Integer> sync_stack = new SynchronisedStack<>();
-		try {
-			for (int i=0;i<10;i++) {
-				sync_stack.push(i);
+	
+	Thread pushThread = new Thread(() -> {
+        for (int i = 1; i <= 10; i++) {
+            sync_stack.push(i);
+            try {
+                Thread.sleep(100);  // Sleep for 500 milliseconds between push operations
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    Thread popThread = new Thread(() -> {
+        for (int i = 1; i <= 12; i++) {
+            try {
+                int value = sync_stack.pop();
+                System.out.println("Popped: " + value);
+                Thread.sleep(1000);  // Sleep for 1000 milliseconds between pop operations
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (EmptyStackException er) {
+				// TODO Auto-generated catch block
+				System.out.println(er.getMessage());
 			}
-			
-			for (Integer e:sync_stack.viewStack()) {
-				System.out.println(e);
-			}
-			
-			for (int j=0;j<11;j++) {
-				System.out.println(sync_stack.pop());
-			}
-		}catch (EmptyStackException e) {
-			System.out.println("Error message: "+e.getMessage());
-			
-		}
-	}
+        }
+    });
+
+    pushThread.start();
+    popThread.start();
+
+}
 }
